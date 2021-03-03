d53f278e2c6b5cf10876817d28a646ea1cc0f0dd
qb <qb@qq.com>
2021-03-03 11:35:08 +0800

    测试手动部署

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+  <p>祝java0821 全体同学前程似锦</p>
+  <p>祝java0821 全体同学财情双手</p>
+  <p>祝java0821 全体同学事业圆满</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
