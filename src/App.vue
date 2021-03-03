27db8021f49e6d9a5ea29d59a876b95dfbe780b7
qb <qb@qq.com>
2021-03-03 14:24:11 +0800

    测试全自动部署

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-  <p>祝java0821 全体同学前程似锦、事业圆满、财情双收！</p>
+  <p>祝java0821 全体同学前程似锦、事业圆满、财情双收！！！</p>
    <img src="https://sh0821-qb.oss-cn-shanghai.aliyuncs.com/avatar/2020/12/24/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20210303115233.jpg
 "/>
   </div>
